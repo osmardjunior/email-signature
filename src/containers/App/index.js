7b64041307920c1d01168a466213fa0b43f92105
Celso Fabri Junior <celso.fabri@live.com>
2020-09-22 17:44:52 -0300

    feat: added react hook form

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {useForm} from 'react-hook-form';
 import Input from 'components/Form/Input';
 
 const App = () => {
+
+  const {handleSubmit, register, errors} = useForm();
+
+  const onSubmit = data => {
+    console.log('data', data);
+  }
+
   return (
     <React.Fragment>
-      <Input type="text" placeholder="Nome" />
+      <Input type="text" placeholder="Nome" ref={register({required: true})} />
     </React.Fragment>
   );
 };
